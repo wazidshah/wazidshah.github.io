9b1603e64273a569ada7a853826e2b3eeb9131f8
wazi <wazid@invenico.co.in>
2018-06-05 11:34:45 +0530

    initial commit no changes

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// var val = 0;
+// var maxVal=50;
+// window.onload = function onLoad() {
+//     var circle = new ProgressBar.Circle('#p1', {
+//         strokeWidth:5,
+//         trailWidth: 0.8,
+//         trailColor: '#f4f4f4',
+//         color: '#FCB03C',
+//         duration: 3000,
+//         easing: 'easeInOut',
+//         text: {
+//             value:'0%'
+//         },
+//         step:function(state, circle){
+//             // circle.path.setAttribute ('stroke', state.color);
+//             // circle.path.setAttribute ('stroke-width', state.width);
+//             var value = Math.round(circle.value() * maxVal);
+//             if (value === 0) {
+//                 circle.setText('');
+//             } else {
+//                 circle.setText(value);
+//             }
+//         }
+//     });
+    
+//     circle.animate(0.5);
+// };
